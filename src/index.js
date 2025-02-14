43cf77a473e2645976403087f682ac8b82b7aede
visionalzone <badkevin@gmail.com>
2025-02-14 14:09:30 -0500

    更新导航栏和页面功能

--- src/index.js
+++ src/index.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// 在文件最后添加
-serviceWorkerRegistration.register({
-  onUpdate: registration => {
-    if (registration.waiting) {
-      if (window.confirm('发现新版本，立即更新？')) {
-        registration.waiting.postMessage({ type: 'SKIP_WAITING' });
-        window.location.reload();
-      }
-    }
-  }
-});
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 如果你不需要性能监控，可以注释掉以下代码
+// reportWebVitals();
